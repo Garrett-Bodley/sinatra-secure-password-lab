df64a592426577d424ff4564014ef9d306986a1f
Garrett <Garrett.Bodley@gmail.com>
2020-12-03 00:19:39 -0500

    commit tree 58218968023ab75edaaaacb097ff655e0a8c7b8c

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,7 @@ configure :development do
 	set :database, {adapter: "sqlite3", database: "db/database.sqlite3"}
 end
 require_relative '../app/controllers/application_controller.rb'
+
+# ActiveRecord::Base.logger.level = 1
+
 require_all 'app/models'
